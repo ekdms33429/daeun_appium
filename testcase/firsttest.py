b8970646c1203a327c21e9b3a3d0c8e588787bde
ekdms33429 <ekdms33429@gmail.com>
2021-11-08 15:41:11 +0900

    commit

--- testcase/firsttest.py
+++ testcase/firsttest.py
@@ -3,18 +3,36 @@ from appium import webdriver
 
 from android.desired_capabilities import get_desired_capabilities
 
-class FirstTest(unittest.TestCase):
+#https://hungc.tistory.com/82
+class TestFirst(unittest.TestCase):
     dc = {}
     driver = None
 
-    def setUp(self):
-        self.dc = get_desired_capabilities("eribank.apk")
-        self.driver = webdriver.Remote("http://localhost:4723/wd/hub", self.dc)
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.dc = get_desired_capabilities("eribank.apk")
+        cls.driver = webdriver.Remote("http://localhost:4723/wd/hub", cls.dc)
+        print("lm setUpClass")
+
+    def setUp(self) -> None:
+        print("lm setUp")
+
+    def test_0001title(self):
+        from android.test_helper import is_alert_exist
+        print("0001")
+        self.assertFalse(is_alert_exist(self.driver))
+
+
+    def test_0002title(self):
+        print("test2")
 
     def test_something(self):
-        print("test")
+        print("test3")
         self.assertEqual(True, True)  # add assertion here
 
+    @classmethod
+    def tearDownClass(cls):
+        print("tearDownClass")
 
 if __name__ == '__main__':
     unittest.main()
