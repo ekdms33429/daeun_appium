25d1c59c1bf5ff255ecca4020808e1490bdb92ad
ekdms33429 <ekdms33429@gmail.com>
2021-11-11 17:42:57 +0900

    commit

--- testcase/firsttest.py
+++ testcase/firsttest.py
@@ -27,37 +27,30 @@ class TestFirst(unittest.TestCase):
         current_activity = self.driver.current_activity
         if current_activity == "com.ctrip.ibu.myctrip.main.module.home.IBUHomeActivity":
             try:
-                WebDriverWait(self.driver, 15).until(EC.presence_of_element_located((By.XPATH, "//*[contains(@content-desc, 'new user page 0 title')]")))
-                print("new user page 0 title")
-                print("len: ", len(titleText_list))
-                for i in range(0, len(titleText_list)-1):
-                    skipBtn = self.driver.find_element_by_accessibility_id("new user page skip button")
-                    title = "new user page "+str(i+1)+" title"
-                    print("i: ", (i+1), " title: ", title)
-                    titleText = self.driver.find_element_by_accessibility_id(title).get_attribute(
-                        "text")
-                    nextText = self.driver.find_element_by_id("ctrip.english:id/nextText").get_attribute("text")
-                    nextBtn = self.driver.find_element_by_accessibility_id("new user page next button")
-                    pager = self.driver.find_element_by_id("ctrip.english:id/pager")
+                page = self.driver.find_element_by_accessibility_id("new user page 0 title")
+                if (page.is_displayed()):
+                    #WebDriverWait(self.driver, 15).until(EC.presence_of_element_located((By.ID, "new user page 0 title")))
+                    print("new user page 0 title")
+                    print("len: ", len(titleText_list))
+                    for i in range(0, len(titleText_list)-1):
+                        skipBtn = self.driver.find_element_by_accessibility_id("new user page skip button")
+                        title = "new user page "+str(i)+" title"
+                        print("i: ", (i+1), " title: ", title)
+                        titleText = self.driver.find_element_by_accessibility_id(title).get_attribute(
+                            "text")
+                        nextText = self.driver.find_element_by_id("ctrip.english:id/nextText").get_attribute("text")
+                        nextBtn = self.driver.find_element_by_accessibility_id("new user page next button")
+                        pager = self.driver.find_element_by_id("ctrip.english:id/pager")
 
-                    self.assertEqual(skipBtn.get_attribute("text"), "건너뛰기")
-                    self.assertTrue(skipBtn.get_attribute("clickable"))
-                    self.assertEqual(titleText.get_attribute("text"), "트립닷컴이 알려주는 할인 항공편 및 호텔 특가 소식")
-                    self.assertEqual(nextText, "다음")
-                    self.assertTrue(nextBtn.get_attribute("clickable"))
-                    self.assertTrue(pager.get_attribute("scrollable"))
+                        self.assertEqual(skipBtn.get_attribute("text"), "건너뛰기")
+                        self.assertTrue(skipBtn.get_attribute("clickable"))
+                        self.assertEqual(titleText.get_attribute("text"), "트립닷컴이 알려주는 할인 항공편 및 호텔 특가 소식")
+                        self.assertEqual(nextText, "다음")
+                        self.assertTrue(nextBtn.get_attribute("clickable"))
+                        self.assertTrue(pager.get_attribute("scrollable"))
 
-                    nextBtn.click()
-                    self.driver.save_screenshot('screencap.png')
-
-
-                content = self.driver.find_element_by_id("ctrip.english:id/contentCard")
-                if(content.is_displayed()):
-                    self.driver.save_screenshot('screencap.png')
-                    #image comparison
-                    closeBtn = self.driver.find_element_by_id("ctrip.english:id/closeIcon")
-                    closeBtn.click()
-                    #홈화면 진입
+                        nextBtn.click()
+                        self.driver.save_screenshot('screencap.png')
 
             except TimeoutException:
                 print("TimeoutException")
@@ -79,6 +72,17 @@ class TestFirst(unittest.TestCase):
                 self.assertEqual(notsubscribeBtn.get_attribute("text"), "나중에")
 
                 subscribeBtn.click()
+
+                """#팝업
+                content = self.driver.find_element_by_id("ctrip.english:id/contentCard")
+                if (content.is_displayed()):
+                    self.driver.save_screenshot('screencap.png')
+                    # image comparison
+                    closeBtn = self.driver.find_element_by_id("ctrip.english:id/closeIcon")
+                    closeBtn.click()
+                    # 홈화면 진입
+                else:
+                    pass"""
 
     def test_0002title(self):
         print("0002")
